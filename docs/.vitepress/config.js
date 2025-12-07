9653e49dbf861e3a1e04d6c77ab8e0d29f2e407e
gqcraftteam <gqcraftsite@gmail.com>
2025-12-07 22:07:07 +0800

    main

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,21 @@
 export default {
-  title: 'My Docs',
-  description: 'My awesome documentation site built with VitePress'
+  title: 'GQcraft Docs',
+  description: 'GQcraft 团队与文档',
+  themeConfig: {
+    logo: '/logo.png',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '文档', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/gqcraftteam/gqcraftdocs' }
+    ],
+    sidebar: [
+      {
+        text: '文档',
+        items: [
+          { text: '快速开始', link: '/guide/' }
+        ]
+      }
+    ]
+  }
 }
+
